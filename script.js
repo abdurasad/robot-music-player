bc86d886e0b1762838facade14fa9553358c2d7d
abdurasad <bijoy3500@gmail.com>
2021-07-04 13:41:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// VoiceRSS Speech Function
+const button = document.getElementById("button");
+const audio = document.getElementById("audio");
+
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, "%20");
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    key: "YOUR_API_KEY_HERE",
+    src: jokeString,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+// Get jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    // Assign One or Two Part Joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Passing Joke to VoiceRSS API
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Get jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    // Assign One or Two Part Joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Passing Joke to VoiceRSS API
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
